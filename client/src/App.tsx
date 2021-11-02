be62434a36c2b35ee0de951bc3cf0ae05f99b416
luizapuccinelli <luizapuccinelli06@gmail.com>
2021-11-02 00:25:02 -0300

    Routers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import { GlobalStyle } from './style-guide/GlobalStyle'
 import { ApolloProvider } from '@apollo/client'
 import client from 'config/apollo-client'
-import Pokedex from 'pages/Pokedex'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from 'pages/Home'
+import Pokedex from 'pages/Pokedex/Pokedex'
 
 const App = () => (
   <ApolloProvider client={client}>
     <GlobalStyle />
-    <Pokedex />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/pokedex">
+          <Pokedex />
+        </Route>
+      </Switch>
+    </Router>
   </ApolloProvider>
 )
 
